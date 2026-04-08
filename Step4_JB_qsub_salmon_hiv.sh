17fc94a91c2103962aee9887329fdd8d87ee27b1
Jessie Buth <35855566+jbuth@users.noreply.github.com>
2026-04-08 14:02:55 -0700

    Update Step4_JB_qsub_salmon_hiv.sh

--- Step4_JB_qsub_salmon_hiv.sh
+++ Step4_JB_qsub_salmon_hiv.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-## submit a loop of qsub jobs to run salmon with hiv transcripts
+## Save this script as:
+## /path/to/HIV-MG-rnaseq/code/Step4_JB_qsub_salmon_hiv.sh
 
-## project folder (base directory - working in scratch folder for now - make sure to download copy)
-BASE_DIR="/u/scratch/j/jbuth/Fregoso_Novitch_Bulk_June2021"
+## To execute, cd into the code folder, then type:
+## ./Step4_JB_qsub_salmon_hiv.sh
+
+## Description:
+## submit a loop of qsub jobs to run salmon with hiv transcripts
+## qsub runs the script Step4a_JB_run_salmon_hiv.sh
+
+## ------ Setup directories ------- ##
+
+## Base directory: /path/to/
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
 ## output Aligned.toTranscriptome.out.bam files from STAR_hiv
 BAM_DIR="${BASE_DIR}/STAR/STAR_hiv"
@@ -22,6 +32,6 @@ for file in "${BAM_DIR}"/*Aligned.toTranscriptome.out.bam; do
     -e "${code}/log" \
     -l h_rt=1:00:00,h_data=8G,highp \
     -pe shared 8 \
-    "${code}/Step4a_JB_run_salmon_hiv.sh" "${name}"
+    "${code}/Step4a_JB_run_salmon_hiv.sh" "${name}" "${BASE_DIR}"
 
 done
