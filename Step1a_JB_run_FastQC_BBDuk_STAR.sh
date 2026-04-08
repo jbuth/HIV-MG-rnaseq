a89f8404a6459d822f95c7a9959d405f1387f6d8
Jessie Buth <35855566+jbuth@users.noreply.github.com>
2026-04-08 14:13:06 -0700

    Update Step1a_JB_run_FastQC_BBDuk_STAR.sh

--- Step1a_JB_run_FastQC_BBDuk_STAR.sh
+++ Step1a_JB_run_FastQC_BBDuk_STAR.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-## project folder (base directory)
-BASE_DIR=/u/scratch/j/jbuth/Fregoso_Novitch_Bulk_June2021
-
-## location of fastq files
-FASTQ_DIR=${BASE_DIR}/fastq
-
-## BBDuk output files (cleaned fastq)
-mkdir -p ${BASE_DIR}/clean_fastq
-CLEAN_FASTQ_DIR=${BASE_DIR}/clean_fastq
-
-## STAR output files 
-BAM_DIR=${BASE_DIR}/STAR/STAR_human
-
 ## full filename was -> S39_d1_4_17_infxn_R848_L001_R1_001.fastq.gz
 ## name -> S39_d1_4_17_infxn_R848
 name="$1"
+
+## project folder (base directory)
+BASE_DIR="$2"
+
+## location of fastq files
+FASTQ_DIR="${BASE_DIR}/fastq"
+
+## BBDuk output files (cleaned fastq)
+mkdir -p "${BASE_DIR}/clean_fastq"
+CLEAN_FASTQ_DIR="${BASE_DIR}/clean_fastq"
+
+## STAR output files 
+BAM_DIR="${BASE_DIR}/STAR/STAR_human"
 
 ###### ----- Clean fastq with BBDuk ----- ######
 
@@ -30,7 +30,7 @@ name="$1"
 module load conda/23.11.0
 conda activate bbmap
 
-## adapters and contaminates to remove 
+## adapters and contaminates to remove (note this was run on hoffman2, edit BBMAP_DIR as needed)
 BBMAP_DIR="/u/home/j/jbuth/.conda/envs/bbmap/opt/bbmap-39.08-0/resources"
 ADAPTERS_fa="${BBMAP_DIR}/adapters.fa"
 ARTIFACTS_fa="${BBMAP_DIR}/sequencing_artifacts.fa.gz"
@@ -54,11 +54,11 @@ conda deactivate
 ###### ----- STAR Alignment with clean fastq file pairs ----- ###### 
 
 ## location of STAR bin & index
-STAR_DIR=/u/project/gandalm/jbuth/bin/STAR/bin/Linux_x86_64/STAR
-STAR_INDEX=/u/project/butlersj/jbuth/RefGenome/homo_sapiens/STAR_index
-	## files to generate STAR index in /u/project/butlersj/jbuth/RefGenome/homo_sapiens
-	## --------> genome.fa -> /Genome/hg38.fa
-	## --------> GTF annotation -> /Annotation/gencode.v28.annotation.gtf
+STAR_DIR="${BASE_DIR}/bin/STAR/bin/Linux_x86_64/STAR"
+STAR_INDEX="${BASE_DIR}/Annotation/homo_sapiens/STAR_index"
+	## files to generate STAR index for Human
+	## --------> genome.fa -> hg38.fa
+	## --------> GTF annotation -> gencode.v28.annotation.gtf
 
 echo "Running STAR alignment on sample: ${name}"
 
@@ -75,18 +75,22 @@ echo "Running STAR alignment on sample: ${name}"
 
 echo "Running FastQC on sample: ${name}"
 
-FASTQC_DIR=/u/project/gandalm/jbuth/bin/FastQC/fastqc
+FASTQC_DIR="${BASE_DIR}/bin/FastQC/fastqc"
+mkdir -p "${BASE_DIR}/fastqc"
 FASTQC_OUTPUT="${BASE_DIR}/fastqc"
 
 ## -- on original fastq files (raw fastq)
+mkdir -p "${FASTQC_OUTPUT}/original_fastq"
 ${FASTQC_DIR} "${FASTQ_DIR}/${name}_L001_R1_001.fastq.gz" --outdir "${FASTQC_OUTPUT}/original_fastq"
 ${FASTQC_DIR} "${FASTQ_DIR}/${name}_L001_R2_001.fastq.gz" --outdir "${FASTQC_OUTPUT}/original_fastq"
 
 ## -- on cleaned fastq files (after BBDuk - input for STAR)
+mkdir -p "${FASTQC_OUTPUT}/clean_fastq"
 ${FASTQC_DIR} "${CLEAN_FASTQ_DIR}/${name}_R1_clean1.fq" --outdir "${FASTQC_OUTPUT}/clean_fastq"
 ${FASTQC_DIR} "${CLEAN_FASTQ_DIR}/${name}_R2_clean2.fq" --outdir "${FASTQC_OUTPUT}/clean_fastq"
 
 ## -- on unmapped fastq files (after STAR alignment)
+mkdir -p "${FASTQC_OUTPUT}/unmapped_fastq"
 ${FASTQC_DIR} "${BAM_DIR}/${name}Unmapped.out.mate1" --outdir "${FASTQC_OUTPUT}/unmapped_fastq"
 ${FASTQC_DIR} "${BAM_DIR}/${name}Unmapped.out.mate2" --outdir "${FASTQC_OUTPUT}/unmapped_fastq"
 
