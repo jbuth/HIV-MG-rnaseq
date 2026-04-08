b04cf4987915afcf3a6066c6c35b3f99ffdec471
Jessie Buth <35855566+jbuth@users.noreply.github.com>
2026-04-08 04:08:39 -0700

    Create Step2a_JB_run_STAR_hiv.sh

---
+++ Step2a_JB_run_STAR_hiv.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## project folder (base directory)
+BASE_DIR="/u/scratch/j/jbuth/Fregoso_Novitch_Bulk_June2021"
+
+## output unmapped fastq files (aligned to human -> saved unmapped)
+UNMAPPED_FASTQ_DIR="${BASE_DIR}/STAR/STAR_human"
+
+## STAR output files 
+BAM_DIR="${BASE_DIR}/STAR/STAR_hiv"
+
+## original -> S39_d1_4_17_infxn_R848Unmapped.out.mate1
+## name -> S39_d1_4_17_infxn_R848
+name="$1"
+
+###### ----- STAR Alignment to HIV with clean fastq file pairs ----- ###### 
+
+## location of STAR bin & index
+STAR_DIR="/u/project/gandalm/jbuth/bin/STAR/bin/Linux_x86_64/STAR"
+STAR_INDEX="${BASE_DIR}/RefGenome/Human_immunodeficiency_virus_1/STAR_index_HIV"
+
+${STAR_DIR} --genomeDir "${STAR_INDEX}" \
+	--readFilesIn "${UNMAPPED_FASTQ_DIR}/${name}Unmapped.out.mate1" "${UNMAPPED_FASTQ_DIR}/${name}Unmapped.out.mate2" \
+	--runThreadN 8 \
+	--outFileNamePrefix "${BAM_DIR}/${name}" \
+	--outSAMtype BAM Unsorted \
+	--quantMode TranscriptomeSAM
+
+## if this was faster than 10 minutes, sleep for the rest
+END=$SECONDS
+ELAPSED=$((END-START))
+if [ "$ELAPSED" -lt 600 ]; then
+  TOSLEEP=$((600 - ELAPSED))
+  sleep $TOSLEEP
+fi
